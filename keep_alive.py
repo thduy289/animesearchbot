3768940271308b8ed134dc4d63876abc3a855c24
thduy289 <168992847+thduy289@users.noreply.github.com>
2026-02-05 17:56:56 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -5,10 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot đang chạy ngon lành!"
+    return "Đang hoạt động..."
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Quan trọng: Render cấp PORT nào thì dùng PORT đó, nếu không có mới dùng 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
